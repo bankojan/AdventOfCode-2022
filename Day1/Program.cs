2e17b5fd1549e70a518e552a205daee8546c9173
banko <banko@JANB>
2022-12-01 22:23:39 +0100

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,30 @@
+﻿var input = File.ReadAllText(".\\Input1.txt");
+
+CalculatePart1(input);
+CalculatePart2(input);
+
+void CalculatePart1(string input)
+{
+    var max = input
+        .Split($"{Environment.NewLine}{Environment.NewLine}")
+        .Select(line => line.Split(Environment.NewLine))
+        .Select(lines => lines.Select(line => int.Parse(line)))
+        .Select(group => group.Sum())
+        .Max();
+
+    Console.WriteLine($"Part 1: {max}");
+}
+
+void CalculatePart2(string input)
+{
+    var sum = input
+        .Split($"{Environment.NewLine}{Environment.NewLine}")
+        .Select(line => line.Split(Environment.NewLine))
+        .Select(lines => lines.Select(line => int.Parse(line)))
+        .Select(group => group.Sum())
+        .OrderByDescending(sum => sum)
+        .Take(3)
+        .Sum();
+
+    Console.WriteLine($"Part 2: {sum}");
+}
