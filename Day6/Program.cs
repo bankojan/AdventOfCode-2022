19ec1dab1eaf3dad230367cd3f5a72625b941e90
bankojan <janbanko996@gmail.com>
2022-12-06 18:38:01 +0100

    Add day 6 solution

---
+++ Day6/Program.cs
@@ -0,0 +1,9 @@
+﻿var text = File.ReadAllText(".\\Input.txt");
+
+Console.WriteLine($"Part 1 solution: {FindStartMarker(text, 4)}");
+Console.WriteLine($"Part 2 solution: {FindStartMarker(text, 14)}");
+
+int FindStartMarker(string text, int n)
+    => text
+        .Select((_, i) => i)
+        .First(index => text.Skip(index).Take(n).ToHashSet().Count == n) + n;
